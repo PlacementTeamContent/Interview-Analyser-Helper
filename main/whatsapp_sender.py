88961799c2634bd408b5b2bd85c7ad3a9e563c3a
PlacementTeamContent <venkatamanojkumar.d@nxtwave.co.in>
2025-04-22 18:07:46 +0530

    Updated message sending.

--- main/whatsapp_sender.py
+++ main/whatsapp_sender.py
@@ -5,5 +5,12 @@ def send_whatsapp_message(phone_number, message):
     # kit.sendwhatmsg_instantly(phone_number, message)
     if phone_number and message:
         whatsapp_url = f"https://web.whatsapp.com/send?phone={phone_number}&text={message}"
-        st.markdown(f'<meta http-equiv="refresh" content="0; url={whatsapp_url}">', unsafe_allow_html=True)
+        # Inject JavaScript to automatically redirect the user to WhatsApp Web
+        redirection_script = f"""
+            <script type="text/javascript">
+                window.location.href = "{whatsapp_url}";
+            </script>
+            """
+
+        st.markdown(redirection_script, unsafe_allow_html=True)
 
