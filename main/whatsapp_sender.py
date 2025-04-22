35c9d8cb69195007c85c761ff2272ff93e7371bf
PlacementTeamContent <venkatamanojkumar.d@nxtwave.co.in>
2025-04-22 18:03:07 +0530

    Updated message sending.

--- main/whatsapp_sender.py
+++ main/whatsapp_sender.py
@@ -1,4 +1,9 @@
-import pywhatkit as kit
+# import pywhatkit as kit
+import streamlit as st
 
 def send_whatsapp_message(phone_number, message):
-    kit.sendwhatmsg_instantly(phone_number, message)
+    # kit.sendwhatmsg_instantly(phone_number, message)
+    if phone_number and message:
+        whatsapp_url = f"https://web.whatsapp.com/send?phone={phone_number}&text={message}"
+        st.markdown(f'<meta http-equiv="refresh" content="0; url={whatsapp_url}">', unsafe_allow_html=True)
+
