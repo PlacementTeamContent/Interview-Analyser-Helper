59ed6ea9c0130d380ef72251900e8f56ad282c27
PST - Content <139737866+PlacementTeamContent@users.noreply.github.com>
2025-04-22 18:25:52 +0530

    Update whatsapp_sender.py

--- main/whatsapp_sender.py
+++ main/whatsapp_sender.py
@@ -5,15 +5,9 @@ def send_whatsapp_message(phone_number, message):
     # kit.sendwhatmsg_instantly(phone_number, message)
     if phone_number and message:
         whatsapp_url = f"https://web.whatsapp.com/send?phone={phone_number}&text={message}"
-        # Inject JavaScript to automatically redirect the user to WhatsApp Web
-        redirection_script = f"""
-            <script type="text/javascript">
-                window.open("{whatsapp_url}", "_blank");
-                console.log("Running...");
-            </script>
-            """
-
-        st.markdown(redirection_script, unsafe_allow_html=True)
+        st.markdown(f"""
+        <a href="{whatsapp_url}" target="_blank">Send Message to {phone_number}</a>
+    """, unsafe_allow_html=True)
     else:
         st.error("Invalid Details")
 
